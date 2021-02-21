a956200542dc13e678b0a1c3040692ab80d5c176
Andrii Nemchenko <andriinemchenko@gmail.com>
2021-02-22 00:11:24 +0200

    review dsl

--- src/main/scala/url.scala
+++ src/main/scala/url.scala
@@ -3,7 +3,7 @@ package ftier
 import annotation._
 import zero.ext._, option._
 
-case class Url(p: P, q: List[Q])
+case class Url(p: P, q: String)
 
 object Url:
   def makep(p: String): P =
@@ -14,33 +14,31 @@ object Url:
       case y :: ys => makep(ys, P.S(y, acc))
       case Nil => acc
 
-  def makeq(q: String): List[Q] =
-    q.split('&').toList.map(_.split('=').toList).collect{
-      case n :: v :: Nil => Q(n, v)}
-
   def unapply(url: String): Option[Url] =
     url.split('?').toList match
       case p :: Nil =>
-        Url(makep(p), nil).some
+        Url(makep(p), "").some
       case p :: q :: Nil =>
-        Url(makep(p), makeq(q)).some
+        Url(makep(p), q).some
       case _ => none
 
 enum P derives CanEqual:
   case S(s: String, n: P)
   case R
 
-case class Q(n: String, v: String)
-
-given CanEqual[Nothing, Q] = CanEqual.derived
-
 object `?`:
-  def unapply(x: String): Option[(P, List[Q])] =
+  def unapply(x: String): Option[(P, String)] =
     Url.unapply(x).map(url => url.p -> url.q)
 
 object `&`:
-  def unapplySeq(xs: List[Q]): Option[List[(String,String)]] =
-    xs.map(q => q.n -> q.v).some
+  def unapplySeq(q: String): Option[List[String]] =
+    q.split('&').toList.some
+
+object `*`:
+  def unapply(x: String): Option[(String, String)] =
+    x.split('=').toList match
+      case a :: b :: Nil => (a -> b).some
+      case _ => none
 
 object `/`:
   def unapply(x: P): Option[(P, String)] =
--- src/test/scala/url.spec.scala
+++ src/test/scala/url.spec.scala
@@ -6,49 +6,37 @@ object UrlSpec extends DefaultRunnableSpec:
   def spec = suite("UrlSpec")(
     test("/") {
       assert("/" match
-        case Url(P.R, Nil) => true
+        case "/" => true
         case _ => false
       )(equalTo(true))
     }
   , test("/a") {
       assert("/a" match
-        case Url(P.S("a", P.R), Nil) => true
+        case Root / "a" => true
         case _ => false
       )(equalTo(true))
     }
   , test("/a/") {
       assert("/a/" match
-        case Url(P.S("a", P.R), Nil) => true
+        case Root / "a" => true
         case _ => false
       )(equalTo(true))
     }
-  , test("/a/b (1)") {
-      assert("/a/b" match
-        case Url(P.S("b", P.S("a", P.R)), Nil) => true
-        case _ => false
-      )(equalTo(true))
-    }
-  , test("/a/b (2)") {
+  , test("/a/b") {
       assert("/a/b" match
         case Root / "a" / "b" => true
         case _ => false
       )(equalTo(true))
     }
-  , test("/a/b?x=1&y=2 (1)") {
-      assert("/a/b?x=1&y=2" match
-        case Url(P.S("b", P.S("a", P.R)), Q("x", "1") :: Q("y", "2") :: Nil) => true
+  , test("/a/b?x=1") {
+      assert("/a/b?x=1" match
+        case (Root / "a" / "b") ? ("x" * "1") => true
         case _ => false
       )(equalTo(true))
     }
-  , test("/a/b?x=1&y=2 (2)") {
+  , test("/a/b?x=1&y=2") {
       assert("/a/b?x=1&y=2" match
-        case (P.R / "a" / "b") ? List(Q("x", "1"), Q("y", "2")) => true
-        case _ => false
-      )(equalTo(true))
-    }
-  , test("/a/b?x=1&y=2 (3)") {
-      assert("/a/b?x=1&y=2" match
-        case (Root / "a" / "b") ? ("x" -> "1" & "y" -> "2") => true
+        case (Root / "a" / "b") ? ("x" * "1" & "y" * "2") => true
         case _ => false
       )(equalTo(true))
     }
